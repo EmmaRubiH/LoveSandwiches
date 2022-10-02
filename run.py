535c510691138e22e3a591025213e61e87e815ac
EmmaRubiH <emmahjorting89@gmail.com>
2022-10-02 15:04:17 +0000

    add data to sales worksheet

--- run.py
+++ run.py
@@ -32,9 +32,8 @@ def get_sales_data():
             print("Data is Valid!")
             break
 
-    return sales_data    
-        
-    
+    return sales_data
+
 def validate_data(values):
     """
     Inside the try, converts all strings values into integers.
@@ -53,4 +52,15 @@ def validate_data(values):
 
     return True
 
+def update_sales_worksheet(data):
+    """
+    update sales worksheet, add new row with the list data provided.
+    """
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
