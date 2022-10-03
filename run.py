7d14f22a72269dcf28fff87615bf68bde2617d28
EmmaRubiH <emmahjorting89@gmail.com>
2022-10-03 07:54:47 +0000

    refactor update worksheet funtions into one

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -48,31 +45,43 @@ def validate_data(values):
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-            )  
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False 
 
     return True
 
-def update_sales_worksheet(data):
-    """
-    update sales worksheet, add new row with the list data provided.
-    """
-    print("updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated successfully.\n")
+
+#def update_sales_worksheet(data):
+#    """
+ #   update sales worksheet, add new row with the list data provided.
+  #  """
+   # print("updating sales worksheet...\n")
+    #sales_worksheet = SHEET.worksheet("sales")
+    #sales_worksheet.append_row(data)
+    #print("sales worksheet updated successfully.\n")
 
 
-def update_surplus_worksheet(data):
+#def update_surplus_worksheet(data):
+ #   """
+  #  update surplus worksheet, add new row with the list data provided.
+   # """
+    #print("updating surplus worksheet...\n")
+    #surplus_worksheet = SHEET.worksheet("surplus")
+    #surplus_worksheet.append_row(data)
+    #print("surplus worksheet updated successfully.\n")
+
+
+def update_worksheet(data, worksheet):
     """
-    update surplus worksheet, add new row with the list data provided.
-    """
-    print("updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated successfully.\n")
+    recives a list of integers to be inserted into a worksheet
+    update the relevant worksheet with the data provided
+    """    
+    print(f"updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -101,10 +110,10 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()    
+main()
